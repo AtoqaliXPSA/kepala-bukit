5bf641940daf4028269ea727a8c99ef79bf6bd11
AtoqaliXPSA <hisyamsafwan60@gmail.com>
2025-06-17 21:58:38 +0000

    Auto update: Tue Jun 17 09:58:38 PM UTC 2025

---
+++ commands/message/ping.js
@@ -0,0 +1,24 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  name: 'ping',
+  description: 'Uji kepantasan respon bot!',
+
+  async execute(message, args, client) {
+    const start = Date.now();
+
+    // Hantar mesej awal
+    const sentMessage = await message.reply('🏓 Mengira ping...');
+
+    const ping = Date.now() - start;
+    const apiPing = client.ws.ping;
+
+    const embed = new EmbedBuilder()
+      .setTitle('🏓 Pong!')
+      .setDescription(`**Bot Ping:** ${ping}ms\n**API Ping:** ${apiPing}ms`)
+      .setColor(0x00AEFF)
+      .setTimestamp();
+
+    await sentMessage.edit({ content: '', embeds: [embed] });
+  }
+};
