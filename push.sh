52ffc6fa70700073c5ca38b0967ffdedb3ec7446
AtoqaliXPSA <youremail@example.com>
2025-06-17 21:19:03 +0000

    Auto update: Tue Jun 17 09:19:03 PM UTC 2025

--- push.sh
+++ push.sh
@@ -1,3 +1,9 @@
-git add .
-git commit -m "Auto update: $(date)"
-git push origin main
+if [ "$RENDER" != "true" ]; then
+  git config user.name "AtoqaliXPSA"
+  git config user.email "youremail@example.com"
+  git add .
+  git commit -m "Auto update: $(date -u)"
+  git push origin main
+else
+  echo "🚫 Skipping push.sh on Render"
+fi
