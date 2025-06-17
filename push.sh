224fb3e12bcb1b8411b82e634aba4be16ccb2051
AtoqaliXPSA <hisyamsafwan60@gmail.com>
2025-06-17 21:22:40 +0000

    Auto update: Tue Jun 17 09:22:40 PM UTC 2025

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 if [ "$RENDER" != "true" ]; then
   git config user.name "AtoqaliXPSA"
-  git config user.email "youremail@example.com"
+  git config user.email "hisyamsafwan60@gmail.com"
   git add .
   git commit -m "Auto update: $(date -u)"
   git push origin main
