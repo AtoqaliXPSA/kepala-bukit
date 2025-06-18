0e16cadcb46e2daecef3448a45d7e22f50bbbc76
AtoqaliXPSA <hisyamsafwan60@gmail.com>
2025-06-18 15:17:26 +0000

    Auto update: Wed Jun 18 03:17:26 PM UTC 2025

---
+++ commands/message/avatar.js
@@ -0,0 +1,22 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  name: 'avatar',
+  description: 'Paparkan avatar pengguna',
+  cooldown: 3, // opsyenal
+
+  async execute(message, args, client) {
+    const user = message.mentions.users.first() || message.author;
+    const avatarUrl = user.displayAvatarURL({ dynamic: true, size: 1024 });
+
+    const embed = new EmbedBuilder()
+      .setTitle(`Avatar ${user.tag}`)
+      .setImage(avatarUrl)
+      .setURL(avatarUrl)
+      .setColor(0x00AEFF)
+      .setFooter({ text: `Diminta oleh ${message.author.tag}`, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
+      .setTimestamp();
+
+    await message.reply({ embeds: [embed] });
+  }
+};
