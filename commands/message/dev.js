9609b1f72cefad54f2a634fc1d4740ab239f65a4
AtoqaliXPSA <hisyamsafwan60@gmail.com>
2025-06-18 15:52:05 +0000

    Auto update: Wed Jun 18 03:52:05 PM UTC 2025

--- commands/message/dev.js
+++ commands/message/dev.js
@@ -71,28 +71,6 @@ module.exports = {
         }
       }
 
-      // ✅ Command: add / remove
-      const target = message.mentions.users.first();
-      if (!target) return message.reply('❌ Sila tag user.');
-
-      const userData = await User.findOneAndUpdate(
-        { userId: target.id },
-        { $setOnInsert: { userId: target.id, balance: 0 } },
-        { upsert: true, new: true }
-      );
-
-      const embed = new EmbedBuilder()
-        .setTitle(`DEV PANEL: ${target.username}`)
-        .setDescription(`Coins semasa: **${userData.balance}**`)
-        .setColor('Gold');
-
-      const row = new ActionRowBuilder().addComponents(
-        new ButtonBuilder().setCustomId(`add_${target.id}`).setLabel('➕ Tambah').setStyle(ButtonStyle.Success),
-        new ButtonBuilder().setCustomId(`remove_${target.id}`).setLabel('➖ Tolak').setStyle(ButtonStyle.Danger)
-      );
-
-      await message.reply({ embeds: [embed], components: [row] });
-
       if (i.customId === 'deploy') {
         exec('node reset.js', (err, stdout, stderr) => {
           if (err) {
