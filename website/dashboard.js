48e7f1cb3122718a53ed0b25f022a2ed23e6a9a2
AtoqaliXPSA <hisyamsafwan60@gmail.com>
2025-06-19 08:30:53 +0000

    Auto update: Thu Jun 19 08:30:53 AM UTC 2025

---
+++ website/dashboard.js
@@ -0,0 +1,17 @@
+// dashboard.js
+
+const user = localStorage.getItem("user");
+if (!user) {
+  window.location.href = "/index.html"; // redirect ke login jika tiada user
+} else {
+  document.getElementById("user").textContent = user;
+}
+
+document.getElementById("statusBtn").addEventListener("click", () => {
+  window.location.href = "/";
+});
+
+document.getElementById("logoutBtn").addEventListener("click", () => {
+  localStorage.removeItem("user");
+  window.location.href = "/index.html";
+});
