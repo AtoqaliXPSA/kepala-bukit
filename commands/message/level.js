76014b029d8148f35d0a8b32ee19639f872c0697
AtoqaliXPSA <hisyamsafwan60@gmail.com>
2025-06-22 19:11:08 +0000

    Auto update: Sun Jun 22 07:11:08 PM UTC 2025

--- commands/message/level.js
+++ commands/message/level.js
@@ -1,16 +1,1 @@
 const Jimp = require('jimp').default;
-const path = require('path');
-
-async function generateLevelCard(username, level, xp) {
-  const bg = await Jimp.read(path.join(__dirname, '../assets/background.png'));
-  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
-
-  bg.print(font, 20, 20, `User: ${username}`);
-  bg.print(font, 20, 60, `Level: ${level}`);
-  bg.print(font, 20, 100, `XP: ${xp}`);
-
-  const buffer = await bg.getBufferAsync(Jimp.MIME_PNG);
-  return buffer;
-}
-
-module.exports = generateLevelCard;
