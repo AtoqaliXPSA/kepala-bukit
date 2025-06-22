5659d46b84b45703f44ff34a4405c306f1e441f7
AtoqaliXPSA <hisyamsafwan60@gmail.com>
2025-06-22 20:26:11 +0000

    Auto update: Sun Jun 22 08:26:11 PM UTC 2025

--- utils/levelCard.js
+++ utils/levelCard.js
@@ -10,12 +10,13 @@ async function generateLevelCard({ username, level, xp, maxXp, rank, avatarURL }
 
   const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
   const avatar = await Jimp.read(avatarURL);
-  avatar.resize(128, 128).circle();
+  avatar.resize(128, 128);
 
-  background.print(font, 180, 30, `Username: ${username}`);
-  background.print(font, 180, 80, `Level: ${level}`);
-  background.print(font, 180, 130, `XP: ${xp} / ${maxXp}`);
-  background.print(font, 180, 180, `Rank: #${rank}`);
+  background.composite(avatar, 30, 30);
+  background.print(font, 180, 30, ` ${username}`);
+  background.print(font, 180, 80, ` Level: ${level}`);
+  background.print(font, 180, 130, ` XP: ${xp} / ${maxXp}`);
+  background.print(font, 180, 180, ` Rank: #${rank}`);
 
   return await background.getBufferAsync(Jimp.MIME_PNG);
 }
