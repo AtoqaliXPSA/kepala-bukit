98b2cc8d783144913ce68d7829f8cd0aba79fa55
AtoqaliXPSA <hisyamsafwan60@gmail.com>
2025-06-22 19:22:18 +0000

    Auto update: Sun Jun 22 07:22:18 PM UTC 2025

--- utils/levelCard.js
+++ utils/levelCard.js
@@ -1,22 +1,17 @@
-const Jimp = require('jimp'); // ✅ gunakan `.default`
+const Jimp = require('jimp');
 
 async function generateLevelCard(username, level, xp) {
-  const width = 800;
-  const height = 250;
-
-  const background = await Jimp.read('background.png'); // tukar ke laluan anda
+  const background = await Jimp.read('background.png');
   const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
 
-  // Optional: bar XP progress
-  const maxXp = level * 100; // contoh kiraan
+  const maxXp = level * 100;
   const progress = Math.min(xp / maxXp, 1);
 
-  // Lukis bar progress secara manual
   background.scan(50, 180, 700 * progress, 20, function (x, y, idx) {
-    this.bitmap.data[idx + 0] = 0;   // R
-    this.bitmap.data[idx + 1] = 255; // G
-    this.bitmap.data[idx + 2] = 0;   // B
-    this.bitmap.data[idx + 3] = 255; // A
+    this.bitmap.data[idx + 0] = 0;
+    this.bitmap.data[idx + 1] = 255;
+    this.bitmap.data[idx + 2] = 0;
+    this.bitmap.data[idx + 3] = 255;
   });
 
   background.print(font, 50, 40, `Username: ${username}`);
