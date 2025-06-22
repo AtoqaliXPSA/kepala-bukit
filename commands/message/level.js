ec38b24f2f267aa7374f36bd57c7bb709e78e53f
AtoqaliXPSA <hisyamsafwan60@gmail.com>
2025-06-22 19:27:48 +0000

    Auto update: Sun Jun 22 07:27:48 PM UTC 2025

--- commands/message/level.js
+++ commands/message/level.js
@@ -1,36 +1,33 @@
 const { AttachmentBuilder } = require('discord.js');
-const generateLevelCard = require('../../utils/levelCard'); // pastikan path betul
-const User = require('../../models/User'); // anda punya model mongoose
+const generateLevelCard = require('../../utils/levelCard');
+const User = require('../../models/User');
 
 module.exports = {
   name: 'level',
-  alias: ['rank', 'xp'],
-  description: 'Lihat level anda.',
+  description: 'Semak tahap XP anda',
   cooldown: 5,
+  async execute(message) {
+    const userData = await User.findOne({ userId: message.author.id });
 
-  async execute(message, args, client) {
-    try {
-      const userId = message.author.id;
-      let user = await User.findOne({ userId });
+    if (!userData) return message.reply('❌ Anda belum ada data XP.');
 
-      // Jika tiada user dalam DB
-      if (!user) {
-        user = await User.create({ userId, balance: 0, lastDaily: null, xp: 0, level: 1 });
-      }
+    const level = userData.level || 1;
+    const xp = userData.xp || 0;
+    const maxXp = level * 100;
+    const rank = 1234; // Contoh, boleh kira ikut leaderboard
 
-      const level = user.level || 1;
-      const xp = user.xp || 0;
+    const avatarURL = message.author.displayAvatarURL({ format: 'png', size: 512 });
 
-      // ✅ Generate level card image
-      const imageBuffer = await generateLevelCard(message.author.username, level, xp);
+    const imageBuffer = await generateLevelCard({
+      username: message.author.username,
+      level,
+      xp,
+      maxXp,
+      rank,
+      avatarURL,
+    });
 
-      const attachment = new AttachmentBuilder(imageBuffer, { name: 'background.png' });
-
-      await message.channel.send({ files: [attachment] });
-
-    } catch (err) {
-      console.error(err);
-      message.reply('❌ Ralat ketika menjana kad level.');
-    }
+    const attachment = new AttachmentBuilder(imageBuffer, { name: 'level.png' });
+    message.channel.send({ files: [attachment] });
   }
 };
