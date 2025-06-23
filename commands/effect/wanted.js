79010ec9b95908844eb12891e75399c24eff71b2
AtoqaliXPSA <hisyamsafwan60@gmail.com>
2025-06-23 22:18:18 +0000

    Auto update: Mon Jun 23 10:18:18 PM UTC 2025

--- commands/effect/wanted.js
+++ commands/effect/wanted.js
@@ -1,5 +1,4 @@
 const { AttachmentBuilder } = require('discord.js');
-const canvacord = require('canvacord');
 
 module.exports = {
   name: 'wanted',
@@ -9,12 +8,15 @@ module.exports = {
   async execute(message, args, client) {
     try {
       const user = message.mentions.users.first() || message.author;
-
       const avatar = user.displayAvatarURL({ format: 'png', size: 512 });
-      const image = await canvacord.Canvas.wanted(avatar);
-      const attachment = new AttachmentBuilder(image, { name: 'wanted.png' });
 
+      // ✅ Import canvacord secara dinamik
+      const canvacord = await import('canvacord');
+      const image = await canvacord.Canvas.wanted(avatar);
+
+      const attachment = new AttachmentBuilder(image, { name: 'wanted.png' });
       await message.reply({ files: [attachment] });
+
     } catch (err) {
       console.error('❌ Error in wanted command:', err);
       message.reply('⚠️ Ralat berlaku semasa menjana gambar "wanted".');
