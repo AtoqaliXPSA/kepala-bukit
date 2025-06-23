8917d4524dd35b0cb65964ef133076a791d414c2
AtoqaliXPSA <hisyamsafwan60@gmail.com>
2025-06-23 19:21:26 +0000

    Auto update: Mon Jun 23 07:21:26 PM UTC 2025

--- utils/cooldownHelper.js
+++ utils/cooldownHelper.js
@@ -2,13 +2,6 @@ const { Collection } = require('discord.js');
 
 const cooldowns = new Collection();
 
-/**
- * Check and apply cooldown.
- * @param {object} message - Discord message object.
- * @param {string} commandName - Nama command.
- * @param {number} cooldownSeconds - Cooldown dalam saat.
- * @returns {boolean} - True jika masih dalam cooldown, false jika boleh teruskan.
- */
 async function checkCooldown(message, commandName, cooldownSeconds) {
   if (!cooldowns.has(commandName)) {
     cooldowns.set(commandName, new Collection());
@@ -24,7 +17,7 @@ async function checkCooldown(message, commandName, cooldownSeconds) {
 
     if (now < expires) {
       const remaining = ((expires - now) / 1000).toFixed(1);
-      const reply = await message.reply(`⏳| **${message.author.displayName}** Sila tunggu ${remaining}s sebelum guna semula.`);
+      const reply = await message.reply(`⏳| **${message.author.username}** Sila tunggu ${remaining}s sebelum guna semula.`);
       setTimeout(() => reply.delete().catch(() => {}), expires - now); // auto delete selepas timeout
       return true; // user masih dalam cooldown
     }
