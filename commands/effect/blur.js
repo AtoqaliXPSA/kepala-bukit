a09319da35547267761a5f15261da83265db3497
AtoqaliXPSA <hisyamsafwan60@gmail.com>
2025-06-23 21:22:51 +0000

    Auto update: Mon Jun 23 09:22:51 PM UTC 2025

--- commands/effect/blur.js
+++ commands/effect/blur.js
@@ -16,7 +16,8 @@ module.exports = {
       const response = await axios.get(avatarURL, { responseType: 'arraybuffer' });
       const avatarBuffer = Buffer.from(response.data); // ❗ PENTING
 
-      const image = await canvacord.Canvas.blur(avatarBuffer);
+      const image = await console.log(typeof canvacord.blur); // jika undefined, maka fungsi memang tiada
+      console.log(typeof canvacord.Canvas?.blur); // alternatif (versi lama)
 
       const attachment = new AttachmentBuilder(image, { name: 'blur.png' });
       await message.reply({ files: [attachment] });
