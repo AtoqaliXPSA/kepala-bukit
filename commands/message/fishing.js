cab254135bd0823e4361d64aa34497f4e2d13cb4
AtoqaliXPSA <hisyamsafwan60@gmail.com>
2025-06-28 21:20:59 +0000

    Auto update: Sat Jun 28 09:20:59 PM UTC 2025

--- commands/message/fishing.js
+++ commands/message/fishing.js
@@ -5,20 +5,11 @@ module.exports = {
   name: 'fishing',
   alias: ['fish'],
   description: '🎣 Pancing ikan dan dapat duit!',
+  cooldown: 5,
+  
   async execute(message, args) {
     const userId = message.author.id;
     const userName = message.author.username;
-
-    // Cooldown (30 saat)
-    const cooldownKey = `fishing_${userId}`;
-    const cooldown = global.cooldowns?.[cooldownKey] || 0;
-    if (Date.now() < cooldown) {
-      const waitTime = Math.ceil((cooldown - Date.now()) / 1000);
-      return message.reply(`⏳ Tunggu ${waitTime}s sebelum memancing semula.`);
-    }
-    global.cooldowns = global.cooldowns || {};
-    global.cooldowns[cooldownKey] = Date.now() + 30000;
-
     // Senarai ikan dan ganjaran
     const fishes = [
       { name: '🐟 Ikan Biasa', min: 10, max: 30 },
