bc9f83d65f0ad41296d2c38c63abafbbf917b4b4
AtoqaliXPSA <hisyamsafwan60@gmail.com>
2025-07-02 17:07:06 +0000

    Auto update: Wed Jul  2 05:07:06 PM UTC 2025

--- commands/message/ping.js
+++ commands/message/ping.js
@@ -3,7 +3,7 @@ const { checkCooldown } = require('../../utils/cooldownHelper');
 module.exports = {
   name: 'ping',
   async execute(message) {
-    const isCooldown = await checkCooldown(message, 'ping', 3);
+    const isCooldown = await checkCooldown(message, 'ping', 2);
     if (isCooldown) return;
 
     const start = Date.now();
