0b761d9849fd55e1a238487e4164e433d573d4f2
AtoqaliXPSA <hisyamsafwan60@gmail.com>
2025-07-02 17:00:39 +0000

    Auto update: Wed Jul  2 05:00:39 PM UTC 2025

--- commands/message/ping.js
+++ commands/message/ping.js
@@ -3,17 +3,18 @@ const { checkCooldown } = require('../../utils/cooldownHelper');
 module.exports = {
   name: 'ping',
   description: 'Uji kepantasan respon bot!',
+  cooldown: 3, // hanya boleh guna sekali setiap 3 saat
 
-  async execute(message, _, client) {
-    const onCooldown = await checkCooldown(message, 'ping', 3);
+  async execute(message, args, client) {
+    const onCooldown = await checkCooldown(message, this.name, this.cooldown);
     if (onCooldown) return;
 
     const start = Date.now();
+    const sent = await message.reply('🏓 Mengira ping...');
 
-    const sentMessage = await message.reply('🏓 Mengira ping...');
     const latency = Date.now() - start;
     const apiPing = Math.round(client.ws.ping);
 
-    await sentMessage.edit(`🏓 Pong! Latency: **${latency}ms**, API: **${apiPing}ms**`);
+    await sent.edit(`🏓 Pong!\n📶 Bot Latency: **${latency}ms**\n🌐 API Ping: **${apiPing}ms**`);
   }
 };
