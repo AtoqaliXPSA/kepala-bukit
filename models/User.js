2db16ffa8b1d1e67b96a41dbf550872c1813941e
AtoqaliXPSA <hisyamsafwan60@gmail.com>
2025-07-24 18:42:54 +0000

    Auto update: Thu Jul 24 06:42:54 PM UTC 2025

--- models/User.js
+++ models/User.js
@@ -1,12 +1,18 @@
 // models/User.js
 const mongoose = require('mongoose');
 
+const itemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  durability: { type: Number, default: 1 },
+  value: { type: Number, default: 0 }
+}, { _id: false });
+
 const userSchema = new mongoose.Schema({
   userId: { type: String, required: true, unique: true },
   xp: { type: Number, default: 0 },
   level: { type: Number, default: 1 },
   balance: { type: Number, default: 0 },
-  inventory: { type: [String], default: [] },
+  inventory: { type: [itemSchema], default: [] },
   stamina: { type: Number, default: 5 },
   lastRegen: { type: Number, default: Date.now },
   lastDaily: { type: Date, default: null }
