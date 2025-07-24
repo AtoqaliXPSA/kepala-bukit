5ccf53c1c4757a5d7cf807a4c4c5865cae361b12
AtoqaliXPSA <hisyamsafwan60@gmail.com>
2025-07-24 19:57:53 +0000

    Auto update: Thu Jul 24 07:57:53 PM UTC 2025

--- commands/message/🔄 economy/buy.js
+++ commands/message/🔄 economy/buy.js
@@ -8,38 +8,64 @@ const shopItems = JSON.parse(fs.readFileSync(itemsPath, 'utf8'));
 
 module.exports = {
   name: 'buy',
-  description: 'Beli item dari shop.',
+  description: 'Beli item dari shop dengan sokongan bulk buy.',
   cooldown: 5,
 
   async execute(message, args) {
-    if (!args[0]) return message.reply('Please specify an item to buy.');
-
-    const itemName = args.join(' ').toLowerCase();
-    const item = shopItems.find(i => i.name.toLowerCase() === itemName);
-
-    if (!item) return message.reply(`Item "${itemName}" not found in shop.`);
-
-    const userId = message.author.id;
-    let user = await User.findOne({ userId });
-
-    if (!user) user = await User.create({ userId, balance: 0, inventory: [] });
-
-    if (user.balance < item.price) {
-      return message.reply(`You don't have enough coins. You need **${item.price} coins**.`);
+    if (!args.length) {
+      return message.reply('**Please specify an item to buy**');
     }
 
-    // Deduct coins and add item
-    user.balance -= item.price;
+    // Semak quantity
+    const quantityArg = parseInt(args[args.length - 1]);
+    const quantity = !isNaN(quantityArg) && quantityArg > 0 ? quantityArg : 1;
 
-    // Add to inventory (as object)
-    user.inventory.push({
-      name: item.name,
-      durability: item.durability,
-      value: item.price
-    });
+    // Dapatkan nama item
+    const itemName = (quantity > 1 ? args.slice(0, -1) : args).join(' ').toLowerCase();
+    const item = shopItems.find(i => i.name.toLowerCase() === itemName);
+
+    if (!item) {
+      return message.reply(`Item **"${itemName}"** not found in shop.`);
+    }
+
+    // Sekatan item
+    if (item.unpurchasable) {
+      return message.reply(`Item **${item.name}** cannot to buy.`);
+    }
+
+    // Harga total
+    const totalPrice = item.price * quantity;
+
+    // Dapatkan user
+    const userId = message.author.id;
+    let user = await User.findOne({ userId });
+    if (!user) {
+      user = await User.create({ userId, balance: 0, inventory: [] });
+    }
+
+    // Semak balance
+    if (user.balance < totalPrice) {
+      return message.reply(
+        `You don't have enough coins . You need **${totalPrice} coins** (price : **${item.price}** x **${quantity}**).`
+      );
+    }
+
+    // Tolak coins
+    user.balance -= totalPrice;
+
+    // Tambah item ke inventory
+    for (let i = 0; i < quantity; i++) {
+      user.inventory.push({
+        name: item.name,
+        durability: item.durability ?? 1,
+        value: item.price
+      });
+    }
 
     await user.save();
 
-    message.reply(`You purchased **${item.name}** for **${item.price} coins**!`);
+    message.reply(
+      `You purchased **${quantity}x ${item.name}** for **${totalPrice} coins**!`
+    );
   }
 };
