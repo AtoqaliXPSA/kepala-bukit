268e9e90bc69712d9cb85757d17403e72dff8745
AtoqaliXPSA <hisyamsafwan60@gmail.com>
2025-08-06 15:47:14 +0000

    Auto update Wed Aug  6 03:47:14 PM UTC 2025

--- commands/message/🔄 economy/fishing.js
+++ commands/message/🔄 economy/fishing.js
@@ -33,8 +33,8 @@ module.exports = {
 
     /* ── Senarai ikan ── */
     const fishOptions = [
-      { name: '🐟 Sardine', chance: 0.55,  minKg: 0.01, maxKg: 0.4,  price: 60  },
-      { name: '🐠 Donny',   chance: 0.035, minKg: 0.4,  maxKg: 1.2,  price: 110 },
+      { name: '🐟 Sardine', chance: 1.55,  minKg: 0.01, maxKg: 0.4,  price: 60  },
+      { name: '🐠 Donny',   chance: 0.35, minKg: 0.4,  maxKg: 1.2,  price: 110 },
       { name: '🦈 Shark',   chance: 0.009, minKg: 10,   maxKg: 60,   price: 30  },
       { name: '🐋 Whale',   chance: 0.001, minKg: 100,  maxKg: 250,  price: 15  }
     ];
